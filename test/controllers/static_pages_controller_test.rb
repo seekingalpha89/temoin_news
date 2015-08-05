ad6c67813deecd8745e0af0a64d361a64a26885d
Alpha Sow <seekingalpha89@gmail.com>
2015-08-04 21:04:19 -0400

    add picture into the assset pipeline

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get about" do
     get :about
     assert_response :success
+      assert_select "title", "Sanarinfo the application for snegalese news"
   end
 
 end
